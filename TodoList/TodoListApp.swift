6db53d38948cf6089c48e32358d88111a0de3f62
Luke Wolfram <lukewolfram05@gmail.com>
2024-07-26 13:54:55 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Luke Wolfram on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
